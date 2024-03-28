8ac846aa2c58fcc78e48e22b18e39bad727d4478
Rohan maharjan <126095500+sinominee@users.noreply.github.com>
2024-03-28 21:06:52 +0545

    Update models.py

--- base/models.py
+++ base/models.py
@@ -6,3 +6,6 @@ class Todo(models.Model):
     name = models.CharField(max_length = 200)
     description = models.TextField()
     status = models.CharField(max_length = 50, choices=[('Done','Done'),('In progress','In progress'),('Not done','Not done')],default='In progress')
+
+     def __str__(self) -> str:
+        return self.name
